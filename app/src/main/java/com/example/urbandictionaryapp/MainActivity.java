75677a0a47fd7310fa558b667dfcfdd7db052d2b
MiłoszSobiecki <53800916+sobusz@users.noreply.github.com>
2020-01-14 01:44:42 +0100

    added the "___" under each element.

--- app/src/main/java/com/example/urbandictionaryapp/MainActivity.java
+++ app/src/main/java/com/example/urbandictionaryapp/MainActivity.java
@@ -67,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
 
                             Log.i("JSON", word+" " +definition);
 
-                            TextToSet = total.append(i+1 + ")  " + definition + "\n\n");
+                            TextToSet = total.append(i+1 + ")  " + definition + "\n ______________________________________ \n \n");
                             WordToSet = word;
                         }
                         Log.d("TOTAL", total.toString());
